da533925b894f26eb3312eb27fbb5082cfbd0ab7
peerpressurre <gsophiay@gmail.com>
2023-04-05 17:20:37 +0300

    k

--- main.cpp
+++ main.cpp
@@ -20,11 +20,35 @@ void quote()
 	Sleep(1500);
 	cout << "\a\a\a\nNa lancyuhu kruzhl’aje tim\"" << endl;
 }
+
+void table()
+{
+	cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << endl;
+	cout << (char)186 << "               Seasons                 " << (char)186 << endl;
+	cout << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)185 << endl;
+
+	cout << (char)201;
+	for (size_t i = 0; i < 39; i++)
+	{
+		cout << (char)205;
+	}	
+	cout << (char)187 << endl;
+	cout << (char)186 << "               Seasons                 " << (char)186 << endl;
+	cout << (char)204;
+	for (size_t i = 0; i < 39; i++)
+	{
+		(i == 8 || i == 16 || i == 24 || i == 32) ? cout << (char)203 : cout << (char)205;
+	}
+	cout << (char)185;
+
+}
+
 int main()
 {
 	info();
 	cout << endl;
 	quote();
 	cout << endl;
+	table();
 	return 0;
 }
